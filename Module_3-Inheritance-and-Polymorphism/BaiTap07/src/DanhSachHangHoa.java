9bebe83a68a3d960aa70950595b694a59ef33e87
Huy Trần <nhokpio95@gmail.com>
2023-06-08 05:01:57 +0700

    Hoàn thành bài 7 module 3

--- Module_3-Inheritance-and-Polymorphism/BaiTap07/src/DanhSachHangHoa.java
+++ Module_3-Inheritance-and-Polymorphism/BaiTap07/src/DanhSachHangHoa.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DanhSachHangHoa {
     private HangHoa[] hangHoaList;
@@ -13,40 +14,34 @@ public class DanhSachHangHoa {
 
     public void them(HangHoa hangHoa) {
         for (int i = 0; i < count; i++) {
-            if (hangHoaList[i].getMaHang().equals(hangHoa.getMaHang()))
+            if (hangHoaList[i].getMaHang().equals(hangHoa.getMaHang())) {
+                System.out.println("Lỗi: Mã hàng hoá này đã có tồn tại trong danh sách");
                 return;
+            }
         }
-        this.hangHoaList[size] = hangHoa;
+        this.hangHoaList[count] = hangHoa;
         count++;
     }
 
     public void inToanBo() {
-        for (int i = 0; i < count; i++) {
-            System.out.println(this.hangHoaList[i]);
-        }
+        System.out.println("Thông tin toàn bộ hàng hoá: ");
+        if (count != 0)
+            for (int i = 0; i < count; i++) {
+                System.out.println(this.hangHoaList[i]);
+            }
+        else System.out.println("Danh sách hàng hoá trống");
     }
 
     public void inTungLoai() {
-        List<HangHoa> hangDienMay = new ArrayList<>();
-        List<HangHoa> hangSanhSu = new ArrayList<>();
-        List<HangHoa> hangThucPham = new ArrayList<>();
+        System.out.println("Thông tin theo từng loại hàng hoá: ");
+        Map<Class<? extends HangHoa>, List<HangHoa>> loaiHangHoaMap = Arrays.stream(hangHoaList)
+                .filter(Objects::nonNull)
+                .collect(Collectors.groupingBy(HangHoa::getClass));
 
-        for (int i = 0; i < count; i++) {
-            if (this.hangHoaList[i] instanceof HangDienMay)
-                hangDienMay.add(this.hangHoaList[i]);
-            else if (this.hangHoaList[i] instanceof HangSanhSu)
-                hangSanhSu.add(this.hangHoaList[i]);
-            else
-                hangThucPham.add(this.hangHoaList[i]);
-        }
-
-        System.out.println("Danh sách hàng điện máy: ");
-        System.out.println(hangDienMay);
-        System.out.println("Danh sách hàng sành sứ: ");
-        System.out.println(hangSanhSu);
-        System.out.println("Danh sách hàng thực phẩm: ");
-        System.out.println(hangThucPham);
-
+        loaiHangHoaMap.forEach((hangHoaClass, hangHoaList) -> {
+            System.out.println("Danh sách hàng " + hangHoaClass.getSimpleName() + ": ");
+            System.out.println(hangHoaList);
+        });
     }
 
     public HangHoa timKiem(String maHang) {
@@ -58,14 +53,30 @@ public class DanhSachHangHoa {
     }
 
     public void sapXepTheoTenTangDan() {
-        Arrays.sort(this.hangHoaList, Comparator.comparing(HangHoa::getTenHang));
+        hangHoaList = Arrays.stream(hangHoaList)
+                .filter(Objects::nonNull)
+                .sorted(Comparator.comparing(HangHoa::getTenHang))
+                .toArray(HangHoa[]::new);
     }
 
     public void sapXepHangHoaTheoSoLuongGiamDan() {
-        Arrays.sort(this.hangHoaList, Comparator.comparing(HangHoa::getSoLuongTon).reversed());
+        hangHoaList = Arrays.stream(hangHoaList)
+                .filter(Objects::nonNull)
+                .sorted(Comparator.comparing(HangHoa::getSoLuongTon).reversed())
+                .toArray(HangHoa[]::new);
+        // Loại bỏ các phần tử null khỏi mảng hangHoaList
+//        HangHoa[] nonNullHangHoaList = Arrays.stream(hangHoaList)
+//                .filter(Objects::nonNull)
+//                .toArray(HangHoa[]::new);
+        // Sắp xếp mảng nonNullHangHoaList theo số lượng tồn giảm dần
+//        Arrays.sort(nonNullHangHoaList, Comparator.comparing(HangHoa::getSoLuongTon).reversed());
+
+        // Gán kết quả vào mảng hangHoaList ban đầu
+//        System.arraycopy(nonNullHangHoaList, 0, hangHoaList, 0, nonNullHangHoaList.length);
     }
 
     public void xuatHangThucPhamKhoBan() {
+        System.out.println("Những thực phẩm có mức độ bán buôn là khó bán: ");
         for (int i = 0; i < count; i++) {
             if(this.hangHoaList[i].mucDoBanBuon() == MucDoBanBuon.KHO_BAN)
                 System.out.println(this.hangHoaList[i]);
@@ -77,12 +88,13 @@ public class DanhSachHangHoa {
         for (int i = 0; i < tempHangHoaList.size(); i++) {
             if (maHang.equals(tempHangHoaList.get(i).getMaHang())) {
                 tempHangHoaList.remove(i);
+                System.out.println("Xoá thành công");
+                this.count = this.count - 1;
+                this.hangHoaList = tempHangHoaList.toArray(new HangHoa[0]);
                 return;
             }
         }
-
-        this.size = tempHangHoaList.size();
-        this.hangHoaList = (HangHoa[]) Arrays.stream(this.hangHoaList).toArray();
+        System.out.println("Không tìm thấy mã hàng " + maHang);
     }
 
     public void suaThongTinDonGia(String maHang) {
@@ -91,6 +103,7 @@ public class DanhSachHangHoa {
             if (maHang.equals(this.hangHoaList[i].getMaHang())) {
                 System.out.println("Nhập đơn giá mới của mã hàng " + maHang + ": ");
                 this.hangHoaList[i].setDonGia(scanner.nextDouble());
+                System.out.println("Sửa đơn giá thành công");
                 return;
             }
         }
--- Module_3-Inheritance-and-Polymorphism/BaiTap07/src/Main.java
+++ Module_3-Inheritance-and-Polymorphism/BaiTap07/src/Main.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
@@ -16,13 +17,13 @@ public class Main {
             System.out.println("5. Tìm kiếm hàng hoá khi biết mã hàng");
             System.out.println("6. Sắp xếp hàng hoá theo tên tăng dần");
             System.out.println("7. Sắp xếp hàng hoá theo số lượng tồn giảm dần");
-            System.out.println("8. Xuất các hàng thực phầm khó bán");
-            System.out.println("9. Xuất các hàng thực phầm khó bán");
-            System.out.println("10. Xoá hàng hoá khi biết mã hàng");
-            System.out.println("11. Sửa thông tin đơn giá khi biết mã hàng");
+            System.out.println("8. Xuất các hàng thực phầm khó bán");;
+            System.out.println("9. Xoá hàng hoá khi biết mã hàng");
+            System.out.println("10. Sửa thông tin đơn giá khi biết mã hàng");
             System.out.println("----");
             System.out.println("0. Thoát chương trình");
-            
+
+            System.out.println("Lựa chọn: ");
             choice = scanner.nextInt();
 
             switch (choice) {
@@ -31,65 +32,130 @@ public class Main {
                     danhSachHangHoa = new DanhSachHangHoa(scanner.nextInt());
                 }
                 case 2 -> {
-                    int loaiHangHoa;
-                    System.out.println("1. Hàng điện máy");
-                    System.out.println("2. Hàng sành sứ");
-                    System.out.println("3. Hàng thực phẩm");
-                    System.out.println("Nhập loại hàng hoá bạn muốn nhập: ");
-                    loaiHangHoa = scanner.nextInt();
+                    if (danhSachHangHoa == null)
+                        System.out.println("Lỗi: Bạn chưa nhập số lượng phần tử cho danh sách");
+                    else {
+                        System.out.println("1. Hàng điện máy");
+                        System.out.println("2. Hàng sành sứ");
+                        System.out.println("3. Hàng thực phẩm");
+                        System.out.println("Nhập loại hàng hoá bạn muốn nhập: ");
+                        int loaiHangHoa = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.println("Nhập mã hàng: ");
+                        String maHang = scanner.nextLine();
+                        System.out.println("Nhập tên hàng: ");
+                        String tenHang = scanner.nextLine();
+                        System.out.println("Nhập đơn giá: ");
+                        double donGia = scanner.nextDouble();
+                        System.out.println("Nhập số luợng tồn: ");
+                        int soLuongTon = scanner.nextInt();
 
-                    String maHang, tenHang;
-                    double donGia;
-                    int soLuongTon;
-
-                    switch (loaiHangHoa) {
-                        case 1 -> {
-                            int thoiGianBaoHanh;
-                            double congSuat;
-                            System.out.println("Nhập mã hàng: ");
-                            maHang = scanner.nextLine();
-                            System.out.println("Nhập tên hàng: ");
-                            tenHang = scanner.nextLine();
-                            System.out.println("Nhập đơn giá: ");
-                            donGia = scanner.nextDouble();
-                            System.out.println("Nhập số luợng tồn: ");
-                            soLuongTon = scanner.nextInt();
-                            System.out.println("Nhập thời gian bảo hành: ");
-                            thoiGianBaoHanh = scanner.nextInt();
-                            System.out.println("Nhập công suất: ");
-                            congSuat = scanner.nextDouble();
-                            danhSachHangHoa.them(new HangDienMay(maHang,tenHang,donGia,soLuongTon,thoiGianBaoHanh,congSuat));
-                        }
-
-                        case 2 -> {
-                            String nhaSanXuat;
-                            LocalDate ngayNhapKho;
-                            int nam;
-                            int thang;
-                            int ngay;
-                            System.out.println("Nhập mã hàng: ");
-                            maHang = scanner.nextLine();
-                            System.out.println("Nhập tên hàng: ");
-                            tenHang = scanner.nextLine();
-                            System.out.println("Nhập đơn giá: ");
-                            donGia = scanner.nextDouble();
-                            System.out.println("Nhập số luợng tồn: ");
-                            soLuongTon = scanner.nextInt();
-                            System.out.println("Nhập nhà sản xuất: ");
-                            nhaSanXuat = scanner.nextLine();
-                            System.out.println("Nhập ngày nhập kho: ");
-                            ngay = scanner.nextInt();
-                            System.out.println("Nhập tháng nhập kho: ");
-                            thang = scanner.nextInt();
-                            System.out.println("Nhập năm nhập kho: ");
-                            nam = scanner.nextInt();
-                            ngayNhapKho = LocalDate.of(nam,thang,ngay);
-                            danhSachHangHoa.them(new HangSanhSu(maHang,tenHang,donGia,soLuongTon,nhaSanXuat,ngayNhapKho));
+                        switch (loaiHangHoa) {
+                            case 1 -> {
+                                int thoiGianBaoHanh;
+                                double congSuat;
+                                System.out.println("Nhập thời gian bảo hành: ");
+                                thoiGianBaoHanh = scanner.nextInt();
+                                System.out.println("Nhập công suất: ");
+                                congSuat = scanner.nextDouble();
+                                danhSachHangHoa.them(new HangDienMay(maHang, tenHang, donGia, soLuongTon, thoiGianBaoHanh, congSuat));
+                            }
+                            case 2 -> {
+                                scanner.nextLine();
+                                System.out.println("Nhập nhà sản xuất: ");
+                                String nhaSanXuat = scanner.nextLine();
+                                System.out.println("Nhập ngày nhập kho (dd/MM/yyyy): ");
+                                String ngayNhapKhoString = scanner.nextLine();
+                                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                                LocalDate ngayNhapKho = LocalDate.parse(ngayNhapKhoString, formatter);
+                                danhSachHangHoa.them(new HangSanhSu(maHang, tenHang, donGia, soLuongTon, nhaSanXuat, ngayNhapKho));
+                            }
+                            case 3 -> {
+                                scanner.nextLine();
+                                System.out.println("Nhập nhà cung cấp: ");
+                                String nhaCungCap = scanner.nextLine();
+                                System.out.println("Nhập ngày sản xuất (dd/MM/yyyy): ");
+                                String ngaySanXuatString = scanner.nextLine();
+                                System.out.println("Nhập ngày hết hạn (dd/MM/yyyy): ");
+                                String ngayHetHanString = scanner.nextLine();
+                                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                                LocalDate ngaySanXuat = LocalDate.parse(ngaySanXuatString, formatter);
+                                LocalDate ngayHetHan = LocalDate.parse(ngayHetHanString, formatter);
+                                danhSachHangHoa.them(new HangThucPham(maHang, tenHang, donGia, soLuongTon, nhaCungCap, ngaySanXuat, ngayHetHan));
+                            }
                         }
                     }
+                }
+                case 3 -> {
+                    if (danhSachHangHoa == null)
+                        System.out.println("Lỗi: Bạn chưa nhập số lượng phần tử cho danh sách");
+                    else
+                        danhSachHangHoa.inToanBo();
+                }
+                case 4 -> {
+                    if (danhSachHangHoa == null)
+                        System.out.println("Lỗi: Bạn chưa nhập số lượng phần tử cho danh sách");
+                    else
+                        danhSachHangHoa.inTungLoai();
+                }
+                case 5 -> {
+                    if (danhSachHangHoa == null)
+                        System.out.println("Lỗi: Bạn chưa nhập số lượng phần tử cho danh sách");
+                    else {
+                        System.out.println("Nhập mã hàng hoá cần tìm kiếm: ");
+                        scanner.nextLine();
+                        String maHangHoa = scanner.nextLine();
+                        HangHoa hangHoaTimDuoc = danhSachHangHoa.timKiem(maHangHoa);
+                        System.out.println("Thông tin hàng hoá có mã " + maHangHoa + ": ");
+                        System.out.println(hangHoaTimDuoc);
+                    }
+                }
+                case 6 -> {
+                    if (danhSachHangHoa == null)
+                        System.out.println("Lỗi: Bạn chưa nhập số lượng phần tử cho danh sách");
+                    else {
+                        danhSachHangHoa.sapXepTheoTenTangDan();
+                        System.out.printf("Danh sách sau khi sắp xếp: ");
+                        danhSachHangHoa.inToanBo();
+                    }
+                }
+                case 7 -> {
+                    if (danhSachHangHoa == null)
+                        System.out.println("Lỗi: Bạn chưa nhập số lượng phần tử cho danh sách");
+                    else {
+                        danhSachHangHoa.sapXepHangHoaTheoSoLuongGiamDan();
+                        System.out.printf("Danh sách sau khi sắp xếp: ");
+                        danhSachHangHoa.inToanBo();
+                    }
+                }
+                case 8 -> {
+                    if (danhSachHangHoa == null)
+                        System.out.println("Lỗi: Bạn chưa nhập số lượng phần tử cho danh sách");
+                    else
+                        danhSachHangHoa.xuatHangThucPhamKhoBan();
 
                 }
-                case 3 -> System.out.println("Goodbye!");
+                case 9 -> {
+                    if (danhSachHangHoa == null)
+                        System.out.println("Lỗi: Bạn chưa nhập số lượng phần tử cho danh sách");
+                    else {
+                        System.out.println("Nhập mã hàng hoá cần xoá: ");
+                        scanner.nextLine();
+                        String maHangHoa = scanner.nextLine();
+                        danhSachHangHoa.xoaHangHoa(maHangHoa);
+                    }
+                }
+                case 10 -> {
+                    if (danhSachHangHoa == null)
+                        System.out.println("Lỗi: Bạn chưa nhập số lượng phần tử cho danh sách");
+                    else {
+                        System.out.println("Nhập mã hàng hoá cần sửa: ");
+                        scanner.nextLine();
+                        String maHangHoa = scanner.nextLine();
+                        danhSachHangHoa.suaThongTinDonGia(maHangHoa);
+                    }
+                }
+                case 0 -> System.out.println("Good bye!!!");
                 default -> System.out.println("Không có lựa chọn này.");
             }
         } while (choice != 0);
